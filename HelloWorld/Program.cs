1b19a766d3b80136c881b7716a88bddc435ae751
Samual King <sik2c@mtmail.mtsu.edu>
2023-02-10 15:28:12 -0600

    Added name prompt.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            string name;
             Console.WriteLine("Hello, World!");
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}!");
         }
     }
 }
